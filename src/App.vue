493461ea1f1276e7dd6bb63d26b70dc0851e80ae
israaelsebaey <israaalsebaei30@gmail.com>
2023-10-25 16:05:36 +0300

    Building Modal

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <h4 class="my-3">Welcome in my website):</h4>
+    <div v-if="showModal">
+      <Modal @close="toggleModal" :theme="modalTheme" @change="changeTheme"/>
+    </div>
+    <button class="btn btn-outline-success" @click="toggleModal">Open Modal</button>
+  </div>
+</template>
+<script>
+  import Modal from './components/Modal.vue';
+  export default {
+    name: 'App',
+    data:()=>({ 
+      showModal:false,
+      modalTheme:'light'
+    }),
+    methods:{
+      toggleModal(){
+        this.showModal=!this.showModal
+      },
+      changeTheme(){
+        if(this.modalTheme=='light'){
+           this.modalTheme='dark'
+        }
+        else if(this.modalTheme=='dark'){
+          this.modalTheme='light'
+        }
+      }
+    },
+    components: {
+      Modal
+    }
+  }
+</script>
+<style scoped>
+  .container{
+    text-align: center;
+    margin-top: 100px;
+  }
+  button{
+    margin: auto;
+    border:1px solid rgb(76, 181, 155) !important;
+    color: rgb(76, 181, 155) !important;
+    transition: 0.5s;
+    height: 40px;
+  }
+  button:hover{
+    background-color: rgb(76, 181, 155) !important;
+    color: #fff !important;
+  }
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="Modal" :class="[theme=='light'?'light':'',theme=='dark'?'dark':'']">
+        <p class="close text" @click.self="closeModal">&times;</p>
+        <h2>Hello, this is a Modal </h2>
+        <p>Lorem ipsum is a placeholder text </p>
+        <p class='changeTheme text' @click="changeTheme">Change theme</p>
+        <div class="btns">
+            <button class="btn btn-outline-primary">Sign up</button>
+            <button class="btn btn-outline-primary mx-3">Log in</button>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name:'Modal',
+        props:['theme'],
+        methods:{
+            closeModal(){
+                this.$emit('close');
+            },
+            changeTheme(){
+                this.$emit('change')
+            }
+        }
+    }
+</script>
+<style scoped>
+   .Modal{
+    width: 400px;
+    margin: auto;
+    text-align: center;
+    background-color: white;
+    box-shadow: 0 0 10px #ddd;
+    border-radius:10px;
+    padding: 30px;
+    position: absolute;
+    top: 200px;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    transition: 1s;
+   }
+   h2{
+    color:rgb(76, 181, 155);
+    margin-top:-30px
+   }
+   p{
+    color: #5e5e5e;
+   }
+   .text{
+    cursor: pointer;
+    color: #5e5e5e;
+   }
+   .text:hover{
+    color:rgb(76, 181, 155);
+   }
+   .light{
+    background-color: #fff;
+   }
+   .dark{
+    background-color: #212121;
+   }
+   .dark p{
+    color: #d5d5d5;
+   }
+   .close{
+    font-size: 25px;
+    text-align: end;
+    position: relative;
+    left:330px;
+    top:-15px;
+    width: 20px;
+   }
+   .changeTheme{
+    width: 110px;
+    margin: auto;
+    margin-top: -10px;
+    margin-bottom: 20px;;
+   }
+   .btns button{
+    border:1px solid rgb(76, 181, 155) !important;
+    color:rgb(76, 181, 155) !important ;
+    height: 40px;
+    width: 80px;
+   }
+   .btns button:hover{
+    background-color: rgb(76, 181, 155) !important;
+    color: #fff !important;
+   }
+</style>
